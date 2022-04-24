e71a23b3bb497b1d712edb80117a1a635aa629e9
AvivHarel <90348080+AvivHarel@users.noreply.github.com>
2022-04-24 16:43:05 +0300

    functions for login form
    the js function to show errors in login form.

---
+++ index.js
@@ -0,0 +1,20 @@
+// show error message.
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
+}
+
+// function to set the error message
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+}
+
+// function to clear the error message
+function clearInputError(inputElement) {
+    inputElement.classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
