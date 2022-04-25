b2d77af53de3c4bfd5dea78dc5e10a4ce30b8be1
AvivHarel <90348080+AvivHarel@users.noreply.github.com>
2022-04-25 16:25:52 +0300

    logout function, and time & date functions

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+function insertTimeMessage(){
+    var today = new Date();
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
 
+
+    return strTime;
+}
+
+function insertDate(){
+    var today = new Date();
+    var date = " "+today.getDate() +"." +(today.getMonth()+1)+'.'+today.getFullYear();
+    return date;
+}
+
+function logout(){
+    window.location.href="index.html";
+}
