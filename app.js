8ed2b614f2f97caa30e6fed195c540980f446633
naoralkobi <92444113+naoralkobi@users.noreply.github.com>
2022-04-25 17:46:02 +0300

    Update app.js
    add function for image

--- app.js
+++ app.js
@@ -18,6 +18,48 @@ function insertDate(){
     return date;
 }
 
+function addImageMessage(image) {
+
+    //var image = document.getElementById('imgOutput');
+    var img = document.createElement('img');
+    img.src = URL.createObjectURL(image[0]);
+
+    img.style.width = "100px";
+    img.style.height = "100px";
+
+    //image.src = URL.createObjectURL(image[0]);
+
+    var table = document.getElementById("myTableData");
+
+    var currentTime = insertTimeMessage();
+
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+
+    let name=document.getElementById("chat_name").innerHTML;
+
+    // in case its a new chat:
+    if (!user_message.has(name)) {
+       user_message.set(name,[]);
+    }
+
+    user_message.get(name)[user_message.get(name).length] = img;
+
+
+    row.insertCell(0).appendChild(img);
+    row.insertCell(1).innerHTML= "<small>"+currentTime+"</small>";
+
+    insertColorsMessage(row);
+    document.getElementById('myImg').value='';
+    
+}
+
+function insertColorsMessage(row){ 
+    //color for the message: 
+    row.cells[0].setAttribute("id","container"); 
+    row.cells[1].setAttribute("class","bla"); 
+}
+
 function logout(){
     window.location.href="index.html";
 }
